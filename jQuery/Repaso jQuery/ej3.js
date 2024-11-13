e9c69005d3d2f746053504ebe0fc468b81c9fc7b
AmaroDel <amarodelaguila04@iesamachado.org>
2024-11-13 12:32:21 +0100

    Hechoo

--- jQuery/Repaso jQuery/ej3.js
+++ jQuery/Repaso jQuery/ej3.js
@@ -1,1 +1,10 @@
 /* Rellena este fichero */
+$(document).ready(function () {
+    // Cuando el sitio web se haya cargado por completo
+    $("#btn-aumentar").click(function () {
+        $(".pares,h1").css({"color": "red", "font-size": "200%"});
+    })
+    $("#btn-disminuir").click(function () {
+        $(".pares,h1").css({"color": "black", "font-size": "50%"});
+    })
+});
