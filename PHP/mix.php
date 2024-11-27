a3879d06068badcaa3a4868eaf06b044fa2be286
AmaroDel <amarodelaguila04@iesamachado.org>
2024-11-27 13:06:23 +0100

    Php

---
+++ PHP/mix.php
@@ -0,0 +1,13 @@
+GNU nano 7.2                         mix.php                                  
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Una web con PHP</title>
+</head>
+<body>
+    <h1>Encabezado</h1>
+    <?php echo "<p>chimpun</p>"; ?>
+</body>
+</html>
