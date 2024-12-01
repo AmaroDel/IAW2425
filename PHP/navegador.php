0332453feca03e8f21833a4c46730766396f2e30
Amaro Del Águila Salas <amarodelaguila04@iesamachado.org>
2024-12-01 17:45:56 +0100

    Ok

--- PHP/navegador.php
+++ PHP/navegador.php
@@ -1,3 +1,15 @@
-<?php
-    echo "Hola usuario, navegas con " . $_SERVER['HTTP_USER_AGENT'] . " desde la dirección"
-php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Una web con php</title>
+</head>
+<body>
+    <h1>Encabezado</h1>
+    <?php 
+        echo "Hola usuario, navegas con " . $_SERVER['HTTP_USER_AGENT'] . " desde la dirección " . $_SERVER['REMOTE_ADDR']; 
+        
+    ?>
+</body>
+</html>
