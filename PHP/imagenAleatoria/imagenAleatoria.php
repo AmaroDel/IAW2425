a5a9df2addf9200d59f62bf4cc69d380950966e8
Amaro Del Águila Salas <amarodelaguila04@iesamachado.org>
2024-12-01 19:15:29 +0100

    PHP pasado

---
+++ PHP/imagenAleatoria/imagenAleatoria.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imagen Aleatoria</title>
+    <style>
+        div{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        img {
+            width: 240px;
+            height: 240px;
+        }
+        h1{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+<?php 
+    $array = array("luffy", "zoro", "nami", "ussop", "sanji", "chopper", "robin", "franky", "brook", "jinbe");
+    $random = rand(0,9);
+    $tripulante = $array[$random];
+?>
+    <h1>Tripulante Aleatorio:</h1><div><img src="<?= $tripulante; ?>.png" alt=""></div>
+
+
+
+</body>
+</html>
---
+++ PHP/index.php
@@ -0,0 +1,10 @@
+<?php
+// Muestra un mensaje de bienvenida
+echo "<h1>¡Bienvenido a tu servidor PHP!</h1>";
+
+// Información sobre la configuración de PHP
+echo "<p>Esta es una prueba para verificar que el servidor Apache con PHP está funcionando correctamente.</p>";
+
+// Mostrar información de PHP
+phpinfo();
+?>
