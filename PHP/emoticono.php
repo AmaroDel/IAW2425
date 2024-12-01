4280035cb118c7bf171eefdb1e3fd013962723d5
Amaro Del Águila Salas <amarodelaguila04@iesamachado.org>
2024-12-01 18:30:07 +0100

    Hechoo

---
+++ PHP/emoticono.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emoticono</title>
+</head>
+<body>
+    <?php
+        $numero = rand(128512,128586);
+    ?>
+    <p>I will display &#<?= $numero ?></p>
+    <!-- 128512 y 128586 -->
+</body>
+</html>
