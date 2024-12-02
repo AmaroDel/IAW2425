e73d205997a7cc859ccd9280ee07b9df9b98f948
AmaroDel <amarodelaguila04@iesamachado.org>
2024-12-02 13:33:11 +0100

    Hecho

---
+++ php/feria.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feria de ¿MAYO?</title>
+</head>
+<body>
+
+<?php 
+$fecha = "6-05-2025";
+$fechahoy =  date("Y-m-d");
+$resta = strtotime($fecha)-strtotime($fechahoy);
+$dias = $resta / (24*3600);
+echo "Quedan " . $dias . " dias "
+?> 
+</body>
+</html>
