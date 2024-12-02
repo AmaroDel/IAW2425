1c5fd0f0bfc9c3dfc39df32464d8885788a96806
AmaroDel <amarodelaguila04@iesamachado.org>
2024-12-02 13:37:01 +0100

    A

--- php/feria.php
+++ php/feria.php
@@ -12,7 +12,7 @@ $fecha = "6-05-2025";
 $fechahoy =  date("Y-m-d");
 $resta = strtotime($fecha)-strtotime($fechahoy);
 $dias = $resta / (24*3600);
-echo "Quedan " . $dias . " dias "
+echo "Quedan " . $dias . " dias para la feria de Sevilla de 2025"
 ?> 
 </body>
 </html>
