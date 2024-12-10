95b9abe1c7ffac6fe1ad32d95bd2b92dc359a355
AmaroDel <amarodelaguila04@iesamachado.org>
2024-12-10 17:09:03 +0100

    A

---
+++ trianguloamaro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangulo</title>
+</head>
+<body>
+    <h1>Triángulo</h1>
+    <form id="formulario" action="triangulo.php">
+	<input type="text" name="numero" id="numero">
+	<input type="submit" value="Enviar">
+</form>
+    <?php
+        if (isset($_GET["numero"])){
+            $numero= htmlspecialchars($_GET["numero"]);
+                if ($numero > 0 && is_numeric($numero)){
+                    for ($i=1;$i<=$numero; $i++){
+                        for($j=1;$j<=$i; $j++){
+                            echo "* "
+                        }
+                        echo "<br>"
+                    }
+                }
+        }
+    ?>
+</body>
+</html>
