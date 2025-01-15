884f56cd7940e19639cd8898110bbb10f43f3113
AmaroDel <amarodelaguila04@iesamachado.org>
2025-01-15 13:09:20 +0100

    hECHO

--- sql/leer1fila.php
+++ sql/leer1fila.php
@@ -2,11 +2,11 @@
 //Conexión con BD
     $servername = "sql308.thsite.top";
     $username = "thsi_38097484";
-    $password = "*****";
+    $password = "A1EbhYLa";
     $bd= "thsi_38097484_ejemplo";
     $enlace= mysqli_connect($servername,$username,$password,$bd);
     if (!$enlace){
-        die("Ocurrión algún problema con la conexión :" . mysqli_connect_error());
+        die("Ocurrió algún problema con la conexión :" . mysqli_connect_error());
     }
 
 //Construcción de la Query
@@ -14,7 +14,14 @@
 //Ejecución de la Query
     $resultado = mysqli_query($enlace, $query);
 //Procesamiento de los resultados
-    print_r($resultado);
+    if (mysqli_num_rows($resultado)>0){
+        //Al menos tengo un registro
+        while($fila = mysqli_fetch_assoc($resultado)){
+            echo "Nombre: " . $fila["nombre"] . " <br>Apellido: " . $fila["apellidos"] . "<br>Email: " . $fila["email"] . "<br>";
+        }
+    }else{
+        echo "<p>No hubo resultados en la tabla</p>";
+    }
 //Cierre de la conexión
     mysqli_close($enlace);
 ?>
