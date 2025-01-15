eb9d9cbd9c4721598eb07797af5210f693484db4
AmaroDel <amarodelaguila04@iesamachado.org>
2025-01-15 13:34:57 +0100

    A

---
+++ sql/actualiza.php
@@ -0,0 +1,26 @@
+<?php
+//Conexión con BD
+    $servername = "sql308.thsite.top";
+    $username = "thsi_38097484";
+    $password = "A1EbhYLa";
+    $bd= "thsi_38097484_ejemplo";
+    $enlace= mysqli_connect($servername,$username,$password,$bd);
+    if (!$enlace){
+        die("Ocurrió algún problema con la conexión :" . mysqli_connect_error());
+    }
+
+//Construcción de la Query
+    $query= "UPDATE usuarios SET nombre='Juan', apellidos='Perez Loperz' WHERE id=4";
+//Ejecución de la Query
+    $resultado = mysqli_query($enlace, $query);
+
+//Procesamiento de los resultados
+    if ($resultado){
+        echo "Registro actualizado correctamente";
+    }
+    else{
+        echo "Error en la actualización";
+    }
+//Cierre de la conexión
+    mysqli_close($enlace);
+?>
---
+++ sql/borra.php
@@ -0,0 +1,26 @@
+<?php
+//Conexión con BD
+    $servername = "sql308.thsite.top";
+    $username = "thsi_38097484";
+    $password = "A1EbhYLa";
+    $bd= "thsi_38097484_ejemplo";
+    $enlace= mysqli_connect($servername,$username,$password,$bd);
+    if (!$enlace){
+        die("Ocurrió algún problema con la conexión :" . mysqli_connect_error());
+    }
+
+//Construcción de la Query
+    $query= "DELETE FROM usuarios WHERE id=4";
+//Ejecución de la Query
+    $resultado = mysqli_query($enlace, $query);
+
+//Procesamiento de los resultados
+    if ($resultado){
+        echo "Delete realizado correctamente";
+    }
+    else{
+        echo "Error en el delete";
+    }
+//Cierre de la conexión
+    mysqli_close($enlace);
+?>
