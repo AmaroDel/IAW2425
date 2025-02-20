94b24d66eb9e39da672cc9f8b1e7f31c61fd6c76
AmaroDel <amarodelaguila04@iesamachado.org>
2025-02-20 14:25:44 +0100

    A

--- proyecto/dashboard.php
+++ proyecto/dashboard.php
@@ -142,8 +142,11 @@ $paginas = ceil($total / $por_pagina); // Calcular total de páginas
     <a href="create.php" class="btn btn-primary me-2">Añadir Nueva Actividad</a>
     <!-- Enlace para cerrar sesión -->
     <a href="logout.php" class="btn btn-danger">Cerrar Sesión</a>
-        <!-- Enlace para cerrar sesión -->
+    <!-- Enlace para cerrar sesión -->
     <a href="descargar.php"><button>Descargar</button></a>
+    <!-- Enlace para modo oscuro -->
+    <a href="oscuro.php" class="modo-oscuro-btn">Activar Modo Oscuro</a>
+
 </div>
 
 <!-- Tabla para mostrar las actividades -->
