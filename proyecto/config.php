a2032c6a25f92594d5dbaf11936542893276740f
Amaro Del Águila Salas <amarodelaguila04@iesamachado.org>
2025-02-23 14:14:04 +0100

    AAAA

--- proyecto/config.php
+++ proyecto/config.php
@@ -12,8 +12,8 @@ if (!$conn) {
 }
 
 // Configuración del correo SMTP con Gmail
-define("SMTP_HOST", "smtp.gmail.com");
-define("SMTP_USER", "wpamarillo@gmail.com"); // Tu correo de Gmail
-define("SMTP_PASS", "phxl tzlz fckw fddu"); // Tu contraseña de aplicación de 16 caracteres
+define("SMTP_HOST", "*******");
+define("SMTP_USER", "*******"); // Tu correo de Gmail
+define("SMTP_PASS", "*******"); // Tu contraseña de aplicación de 16 caracteres
 define("SMTP_PORT", 587);
 ?>
